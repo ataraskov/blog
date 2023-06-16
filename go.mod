bd27ed87627229dd456347f98b46a3a436a7c1c1
Alexey Taraskov <alexey.taraskov@gmail.com>
2023-06-16 12:54:21 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module testing-regexp
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	question = "question"
+	empty    = "empty"
+	other    = "other"
+)
+
+func main() {
+	fmt.Println("Ok")
+}
